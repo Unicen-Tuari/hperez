ada038f90ee0eb6d1ccbf78ca08028ef0058ecef
perezhgr <perez.hgr@gmail.com>
2014-10-26 16:37:29 -0300

    Cuando comentabas una ciudad (usando Ajax) se cerraba el templates del formulario y los comentarios (Problema Solucionado).fixed #38

--- TuparVyT/controllers/controller_grabar_comentario.php
+++ TuparVyT/controllers/controller_grabar_comentario.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 class ControllerGrabarComentario
 {
 	private $model;
@@ -8,8 +9,7 @@ class ControllerGrabarComentario
 		$this->model = $model;
 		$this->view = $view;
 	}
-
-
+/*
 	public function GrabarComentario(){
 
 		if (isset($_POST['texto'])) {
@@ -20,22 +20,30 @@ class ControllerGrabarComentario
 			
 			$this->model->InsertaComentario($comentario);
 		}
-	}
+	}*/
 
-	public function BuscarComentario(){
+		public function ImprimirComentario(){
 
-		if (isset($_GET['id_ciudad'])) {
-			$this->view->MostrarUltimoComentarioInsertado($this->model->BuscarComentario($_GET['id_ciudad']));
+			if (isset($_POST['texto'])) {
+			$comentario["texto"] = $_POST["texto"];	
+			$comentario["id_usuario"] = $_POST["id_usuario"];
+			$comentario["id_ciudad"] = $_POST["id_ciudad"];
+			$comentario["id_condicion"] = $_POST["id_condicion"];
+			
+			$this->model->InsertaComentario($comentario);
+		}
+
+			$this->view->MostrarComentario($this->model->ObtenerComentarioByIdCiudad($comentario["id_ciudad"]),$this->model->ObtenerCountComentarioByIdCiudad($comentario["id_ciudad"]));
+			$this->view->MostrarCondicionFormularioComentario($this->model->ObtenerCondicion());			
+			$this->view->MostrarIdCiudadFormularioComentario($this->model->ObtenerIdCiudad($comentario["id_ciudad"]));			
+		
+
+		if (isset($_SESSION['nombre'])) {
+			$this->view->ImprimirComentarioSesion($_SESSION["nombre"],$_SESSION["id_usuario"]);	
+		}
+		else{
+			$this->view->ImprimirComentario();	
 		}
 	}
-
-	public function ObtenerCountComent(){
-
-		if (isset($_GET['id_ciudad'])) {
-			$this->view->MostrarCountComentarioActualizado($this->model->ObtenerCountComentarioActualizado($_GET['id_ciudad']));
-		}
-	}
-
-	
 }
 ?>
